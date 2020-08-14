07ca797bc551a63da39061cc40d9269126e6d72a
Leonardo Faria <leonardofaria@gmail.com>
2020-08-13 22:22:17 -0700

    Fix NavLink class names

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -9,7 +9,7 @@ const Layout: FunctionComponent = ({ children }) => {
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 font-inter">
-      <header className="bg-black fixed z-10 w-full border-gray-700 border-b">
+      <header className="bg-black bg-opacity-75 fixed z-10 w-full border-gray-700 border-b">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-inter-3xl text-white h-16 flex items-center">
             Lights, camera, action
@@ -17,7 +17,7 @@ const Layout: FunctionComponent = ({ children }) => {
 
           <ul className="w-full flex flex-row h-12 items-end">
             <li className="h-12 flex items-end">
-              <NavLink activeClassName="border-white" className={linkClassName} to="/">
+              <NavLink activeClassName="border-white" className={linkClassName} to="/" exact>
                 Dashboard
               </NavLink>
             </li>
