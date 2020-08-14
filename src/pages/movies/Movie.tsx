c850e0a124b164ab8a0e30ff208d3588087901da
Leonardo Faria <leonardofaria@gmail.com>
2020-08-14 00:18:33 -0700

    Update movie page

--- src/pages/movies/Movie.tsx
+++ src/pages/movies/Movie.tsx
@@ -1,6 +1,7 @@
-import React, { FunctionComponent, Suspense } from 'react';
-import { createResource } from '../../api/suspense';
+import React, { useState, useEffect, FunctionComponent } from 'react';
 import Loader from '../../components/Loader';
+import { fetchAPIData } from '../../api/api';
+import { useAPI } from '../../api/context';
 
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 
@@ -8,31 +9,46 @@ interface MatchParams {
   id: string;
 }
 
-interface MatchProps extends RouteComponentProps<MatchParams> {
+interface MatchProps extends RouteComponentProps<MatchParams> {}
+
+interface Movie {
+  title: string,
+  genres: string[],
+  poster_path: string,
+  overview: string,
 }
 
-const test = (id: String) => {
+const Movies: FunctionComponent<MatchProps> = ({ children, match }) => {
+  const contextAPI = useAPI();  
+  // console.dir(contextAPI?.showsGenres);
 
-  return createResource(`movie/${id}`, '');
-}
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [loading, setLoading] = useState(false);
 
-type Props = {
-  item?: any,
-}
+  useEffect(() => {
+    setLoading(true);
+    fetchAPIData(`movie/${match.params.id}`, null).then(
+      (response: Movie) => {
+        setMovie(response);
+        setLoading(false);
+      }
+    );    
+  }, [match.params.id]);
 
-const Movies: FunctionComponent<Props> = ({ children, item }) => {
-  // const item = test(match.params.id).fetchData.read();
-  console.dir(item.fetchData.read());
+  if (loading) {
+    return (<Loader message="Loading movie" />)
+  }
+
+  console.log(movie);
+
   return (
     <>
-      <h2 className="text-inter-2xl text-white mb-5">Popular movies</h2>
+      <h2 className="text-inter-2xl text-white mb-5">{movie?.title}</h2>
 
-      <Suspense fallback={<Loader message="Loading movie" />}>
-        Hi
-      </Suspense>
+      <p>{movie?.overview}</p>
     </>
     
   );
 };
 
-export default Movies;
+export default withRouter(Movies);
