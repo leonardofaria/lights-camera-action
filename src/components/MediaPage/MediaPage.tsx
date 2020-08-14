97e3a69c7817a040ae573f93d57247aa07ae57be
Leonardo Faria <leonardofaria@gmail.com>
2020-08-14 15:40:41 -0700

    Add tabs in MediaPage with more information about the media

--- src/components/MediaPage/MediaPage.tsx
+++ src/components/MediaPage/MediaPage.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent} from 'react';
+import { useTabState, Tab, TabList, TabPanel } from "reakit/Tab";
 import { IMAGE_PATH, IMAGE_SIZE } from '../../constants';
 import Genres from '../Genres';
 
@@ -8,24 +9,72 @@ type Props = {
 }
 
 const ItemPage: FunctionComponent<Props> = ({ item, mediaType }) => {
-  
+  const tab = useTabState();
+
   return (
-    <section className="flex">
-      <div className="w-full sm:w-1/2 md:w-1/3">
-        <div className="mr-6">
-          <img src={`${IMAGE_PATH}/${IMAGE_SIZE}/${item?.poster_path}`} alt={item?.title} className="w-full h-auto" />
+    <>
+      <section className="flex">
+        <div className="w-full sm:w-1/2 md:w-1/3">
+          <div className="mr-6">
+            <img src={`${IMAGE_PATH}/${IMAGE_SIZE}/${item?.poster_path}`} alt={item?.title} className="w-full h-auto" />
+          </div>
         </div>
-      </div>
 
-      <div className="w-full sm:w-1/2 md:w-2/3">
-        <h2 className="text-inter-3xl text-white mb-5">{item?.title}{item?.name}</h2>
+        <div className="w-full sm:w-1/2 md:w-2/3">
+          <h2 className="text-inter-3xl text-white mb-5">{item?.title}{item?.name}</h2>
 
-        <Genres genres={item?.genres} mediaType={mediaType} />
+          <Genres genres={item?.genres} mediaType={mediaType} />
 
-        <p>{item?.overview}</p>
+          <p>{item?.overview}</p>
 
-      </div>
-    </section>
+        </div>
+      </section>
+
+      <section className="flex mt-6">
+        <div className="w-full sm:w-1/2">
+          <div className="mr-6">
+            <TabList {...tab}>
+              <Tab className="border-gray-700 border-l border-t border-r rounded-t py-2 px-4 text-gray-200" {...tab}>Details</Tab>
+              <Tab className="border-gray-700 border-l border-t border-r rounded-t py-2 px-4 text-gray-200" {...tab}>Cast</Tab>
+            </TabList>
+            <TabPanel className="border-gray-700 border" {...tab}>
+              <table className="table-auto">
+                <tbody>
+                  <tr className="w-full">
+                    <td className="px-4 py-2">
+                      Vote average
+                    </td>
+                    <td className="px-4 py-2">
+                      { item?.vote_average } / 10
+                    </td>
+                  </tr>
+                  <tr className="w-full">
+                    <td className="px-4 py-2">
+                      Release date
+                    </td>
+                    <td className="px-4 py-2">
+                      { item?.release_date }
+                    </td>
+                  </tr>
+                  <tr className="w-full">
+                    <td className="px-4 py-2">
+                      Revenue
+                    </td>
+                    <td className="px-4 py-2">
+                      { item?.revenue }
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </TabPanel>
+            <TabPanel className="border-gray-700 border py-2 px-4" {...tab}>
+              Cast - todo
+            </TabPanel>
+          </div>
+        </div>
+      </section>
+    </>
+    
   );
 };
 
