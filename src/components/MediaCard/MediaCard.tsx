3a9777c3926540fddb37c5c5e9e3f614adc49f39
Leonardo Faria <leonardofaria@gmail.com>
2020-08-14 15:52:22 -0700

    Add hover state in card

--- src/components/MediaCard/MediaCard.tsx
+++ src/components/MediaCard/MediaCard.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent} from 'react';
+import React, { useState, FunctionComponent} from 'react';
 import { 
   Link
 } from "react-router-dom";
@@ -10,9 +10,17 @@ type Props = {
 }
 
 const ItemCard: FunctionComponent<Props> = ({ item, mediaType }) => {
-  const path = mediaType === "trending" ? item.media_type.replace("tv", "show") : mediaType;
+  const type = mediaType === "trending" ? item.media_type.replace("tv", "show") : mediaType;
+  const [showDetails, setShowDetails] = useState<boolean>(false);
+
   return (
-    <Link to={`/${path}s/${item.id}`}>
+    <Link className="relative" to={`/${type}s/${item.id}`} onMouseEnter={() => setShowDetails(true)} onMouseLeave={() => setShowDetails(false)}>
+      { 
+        showDetails && 
+        <div className="absolute w-full h-full bg-black bg-opacity-50 color-white flex p-4 text-xl items-center text-center">
+          { type === "movie" ? item.title : item.name }
+        </div>
+      }
       <img src={`${IMAGE_PATH}/${IMAGE_SIZE}/${item.poster_path}`} alt={item.title} />
     </Link>
   );
