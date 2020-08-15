f1883305bd5fa55cc0a681111c2aa083cf94a6ce
Leonardo Faria <leonardofaria@gmail.com>
2020-08-15 13:01:45 -0700

    Add neomorphism-in-tailwind package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,42 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    neomorphism: {
+			sources: {
+				"l": 0,
+				"lt": Math.PI * (1 / 4),
+				"t": Math.PI * (1 / 2),
+				"rt": Math.PI * (3 / 4),
+				"r": Math.PI,
+				"rb": Math.PI * (5 / 4),
+				"b": Math.PI * (3 / 2),
+				"lb": Math.PI * (7 / 4),
+			},
+			distances: {
+				"xs": "3px",
+				"sm": "7px",
+				"base": "11px",
+				"md": "15px",
+				"lg": "20px",
+				"xl": "25px",
+				"2xl": "30px",
+				"3xl": "35px",
+				"4xl": "40px",
+				"5xl": "45px",
+				"6xl": "50px"
+			},
+			blurs: {
+				"sm": (value) => `calc(1 * var(${value}))`,
+				"base": (value) => `calc(2 * var(${value}))`,
+				"md": (value) => `calc(3 * var(${value}))`
+			}
+		},
   },
-  variants: {},
+  variants: {
+    neomorphism: ['responsive', 'hover', 'focus', 'active'],
+  },
   plugins: [
-    require('tailwindcss-font-inter')()
+    require('tailwindcss-font-inter')(),
+    require('neomorphism-in-tailwind')
   ],
 }
